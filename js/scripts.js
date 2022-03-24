e5debc11efe5d4d1a550c072ae5f531101befad0
JeremyM45 <jeremyvs1945@gmail.com>
2022-03-24 15:24:59 -0700

    add logic to radio selectors that allow for all possible cominations to have unique answer
    Co-authored-by: Evgeny Zbirun <zbirunzbirun@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,27 @@ $(document).ready(function() {
     event.preventDefault();
     
     const operator1 = $("input:radio[name=operator1]:checked").val();
+    const operator2 = $("input:radio[name=operator2]:checked").val();
+    const operator3 = $("input:radio[name=operator3]:checked").val();
+
     let result;
-    if (operator1 === "answer1") {
-      result = "answer1"
-     } else if (operator1 === "answer2") {
-       result = "answer2"
-     } else if (operator1 === "answer3") {
-       result = "answer3"
-     } else if (operator1 === "answer4") {
-       result = "answer4"
+    if (operator1 === "answer1" && operator2 === "answer1" && operator3 === "answer1") {
+      result = "result 1"
+     }  else if (operator1 == "answer1" && operator2 === "answer2" && operator3 === "answer1") {
+      result = "result2";
+     }  else if (operator1 == "answer1" && operator2 === "answer1" && operator3 === "answer2") {
+      result = "result3";
+     }  else if (operator1 == "answer1" && operator2 === "answer2" && operator3 === "answer2") {
+      result = "result4";
+     }
+     if (operator1 == "answer2" && operator2 === "answer1" && operator3 === "answer1") {
+      result = "result5";
+     }  else if (operator1 == "answer2" && operator2 === "answer2" && operator3 === "answer1") {
+      result = "result6";
+     }  else if (operator1 == "answer2" && operator2 === "answer1" && operator3 === "answer2") {
+      result = "result7";
+     }  else if (operator1 == "answer2" && operator2 === "answer2" && operator3 === "answer2") {
+      result = "result8";
      }
     $("#output").text(result);
   });
